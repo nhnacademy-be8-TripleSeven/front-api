18f4926760bb9dc740b4e4f87e8a9760e1c64975
wjpark4430 <pwj4na2@gmail.com>
2024-12-19 15:09:47 +0900

    🎉 Begin a project

---
+++ src/main/java/com/tripleseven/frontapi/controller/admin/AdminCouponController.java
@@ -0,0 +1,25 @@
+package com.tripleseven.frontapi.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/frontend")
+public class AdminCouponController {
+
+    @GetMapping("/coupon-policy/create")
+    public String createCouponPolicy() {
+        return "/admin/coupon-policy/create";
+    }
+
+    @GetMapping("/coupon-policy/detail")
+    public String getCouponPolicyPage() {
+        return "/admin/coupon-policy/detail";
+    }
+
+    @GetMapping("/coupons/manage")
+    public String getCouponManagePage() {
+        return "/admin/coupon/manage";
+    }
+}
---
+++ src/main/java/com/tripleseven/frontapi/controller/user/UserController.java
@@ -0,0 +1,63 @@
+package com.tripleseven.frontapi.controller.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/api/frontend/member/my-page/info")
+    public String getMyPage() {
+        return "/member/my-page/info";
+    }
+
+    @GetMapping("/frontend/guest/orders/search")
+    public String searchGuestOrder() {
+        return "/guest/order/search";
+    }
+
+    @GetMapping("/frontend/guest/orders")
+    public String getGuestOrderDetails() {
+        return "/guest/order/detail";
+    }
+
+    @GetMapping("/frontend/guest/find/order-number")
+    public String findGuestOrderNumbers() {
+        return "/guest/search/order-number";
+    }
+
+    @GetMapping("/frontend/guest/find/order-password")
+    public String findGuestOrderPassword() {
+        return "/guest/search/order-password";
+    }
+
+    @GetMapping("/frontend/member/my-page/order/manage")
+    public String manageMemberOrders() {
+        return "/member/my-page/order/manage";
+    }
+
+    @GetMapping("/frontend/member/my-page/order/refund-exchange")
+    public String getRefundExchangeDetails() {
+        return "/member/my-page/order/refund-exchange";
+    }
+
+    @GetMapping("/frontend/member/my-page/like")
+    public String getMyLikeBooks() {
+        return "/member/my-page/like/like";
+    }
+
+    @GetMapping("/frontend/member/my-page/points/use")
+    public String getMyPointUseHistory() {
+        return "/member/my-page/point-history/use";
+    }
+
+    @GetMapping("/frontend/member/my-page/points/earn")
+    public String getMyPointEarnHistory() {
+        return "/member/my-page/point-history/earn";
+    }
+
+    @GetMapping("/frontend/member/my-page/coupons")
+    public String getCouponDetails() {
+        return "/member/my-page/coupon/detail";
+    }
+}
