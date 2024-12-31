b68864343454584d4e33088a6a17c3f2d6a27ece
lushlife99 <wjdcks282828@gmail.com>
2024-12-31 15:34:39 +0900

    ci : ci/cd시 SKM 설정 추가

---
+++ src/main/java/com/tripleseven/frontapi/error/KeyManagerException.java
@@ -0,0 +1,7 @@
+package com.tripleseven.frontapi.error;
+
+public class KeyManagerException extends RuntimeException {
+    public KeyManagerException(String message) {
+        super(message);
+    }
+}
