814d22ad7e5611f901c383cb07f022f1e6241b30
lushlife99 <wjdcks282828@gmail.com>
2024-12-31 15:35:30 +0900

    feat(payco-login) : 페이코 로그인 기능 추가

--- src/main/java/com/tripleseven/frontapi/controller/auth/AuthController.java
+++ src/main/java/com/tripleseven/frontapi/controller/auth/AuthController.java
@@ -1,11 +1,21 @@
 package com.tripleseven.frontapi.controller.auth;
 
+import com.tripleseven.frontapi.annotations.secure.SecureKey;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 @Controller
 public class AuthController {
+
+    @SecureKey("secret.keys.payco.client-id")
+    private String paycoClientId;
+    @SecureKey("secret.keys.payco.client-secret")
+    private String paycoClientSecret;
 
     @GetMapping("/join")
     public String join() {
@@ -14,8 +24,28 @@ public class AuthController {
     }
 
     @GetMapping("/login")
-    public String login() {
+    public String login(ModelAndView modelAndView) {
+        modelAndView.addObject("paycoClientId", paycoClientId);
+        modelAndView.setViewName("auth/login");
         return "auth/login";
+    }
+
+    @GetMapping("/oauth2/authorization/payco")
+    public String paycoLoginRedirect() {
+        String redirectUri = "https://nhn24.store/backend/auth/payco/callback";
+        String encodedRedirectUri;
+        try {
+            encodedRedirectUri = URLEncoder.encode(redirectUri, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException("Error encoding redirectUri", e);
+        }
+        String authorizationUri = "https://id.payco.com/oauth2.0/authorize"
+                + "?response_type=code"
+                + "&client_id=" + paycoClientId
+                + "&redirect_uri=" + encodedRedirectUri
+                + "&scope=profile";
+
+        return "redirect:" + authorizationUri;
     }
 
     @GetMapping("/account/find")
