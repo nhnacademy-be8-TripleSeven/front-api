5cfbb031c392c760ea12a9cc063e77d66fdf6d56
kimkihyun <kkhkh4531@gmail.com>
2025-01-02 18:45:06 +0900

    feat:리뷰 이미지 등록

--- src/main/java/com/tripleseven/frontapi/dto/review/ReviewRequestDTO.java
+++ src/main/java/com/tripleseven/frontapi/dto/review/ReviewRequestDTO.java
@@ -10,12 +10,14 @@ public class ReviewRequestDTO {
     private Long bookId;
     private int rating;
     private String text;
+    private String imageUrl;
 
     public ReviewRequestDTO() {}
 
-    public ReviewRequestDTO(Long bookId, int rating, String text) {
+    public ReviewRequestDTO(Long bookId, int rating, String text, String imageUrl) {
         this.bookId = bookId;
         this.rating = rating;
         this.text = text;
+        this.imageUrl = imageUrl;
     }
 }
