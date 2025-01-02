c2878b54ad4f4ce024b0599a3fcec4957a0ea0f9
lushlife99 <wjdcks282828@gmail.com>
2025-01-02 17:09:22 +0900

    fix : 라우팅 경로 추가

--- src/main/resources/static/js/payco-callback.js
+++ src/main/resources/static/js/payco-callback.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const clientId = clientIdElement.getAttribute('data-login-id');
 
     if (clientId) {
-        axios.post(`/backend/auth/payco/login`, null, {
+        axios.post(`/auth/payco/login`, null, {
 
             params: {
                 clientId : clientId
@@ -16,15 +16,15 @@ document.addEventListener('DOMContentLoaded', function () {
                 // JWT를 로컬스토리지에 저장
                 setToken(tokenInfo.accessToken);
                 // 메인 페이지로 리디렉션
-                window.location.href = '/main';
+                window.location.href = '/frontend/main';
             })
             .catch(error => {
                 console.error('로그인 요청 중 오류 발생:', error);
-                window.location.href = '/login'; // 실패 시 로그인 페이지로 리디렉션
+                window.location.href = '/frontend/login'; // 실패 시 로그인 페이지로 리디렉션
             });
     } else {
         alert('클라이언트 ID가 제공되지 않았습니다. 로그인 페이지로 돌아갑니다.');
-        window.location.href = '/login';
+        window.location.href = '/frontend/login';
     }
 });
 
